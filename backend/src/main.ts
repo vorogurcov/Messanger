819cb65b36e05f9dc81546d811d933742792ffc1
vorogurcov <vania.frol2010@yandex.ru>
2025-03-03 17:41:47 +0300

    Add validation to dto objects

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import {NestExpressApplication} from "@nestjs/platform-express";
+import {ValidationPipe} from "@nestjs/common";
 async function bootstrap() {
   console.log(process.env.DB_PROVIDER)
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   const corsOptions = {
     origin: `http://${process.env.FRONTEND_HOST}:${process.env.FRONTEND_PORT}`,
