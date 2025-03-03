39c43def930e22e3c731f050749eb3188449ad92
vorogurcov <vania.frol2010@yandex.ru>
2025-03-03 11:31:31 +0300

    Add UserRepository functionality

--- backend/src/auth/repositories/user.repository.ts
+++ backend/src/auth/repositories/user.repository.ts
@@ -3,6 +3,7 @@ import {Repository} from "typeorm";
 import {Injectable} from '@nestjs/common'
 import {User} from '../entities/user.entity'
 import {RegisterUserDto} from "../dto/register-user.dto";
+import {LoginUserDto} from "../dto/login-user.dto";
 
 @Injectable()
 export class UserRepository{
@@ -11,8 +12,32 @@ export class UserRepository{
         private repository:Repository<User>
     ){}
 
-    async createUser(registerUserDto:RegisterUserDto){
-        const user = await this.repository.create(registerUserDto)
-        await this.repository.save(user)
+    async saveUser(registerUserDto:RegisterUserDto){
+        try{
+            const user = await this.repository.create(registerUserDto)
+            await this.repository.save(user)
+        }catch(error:any){
+            console.error('Can not save user!', error.message)
+            throw error;
+        }
     }
+    async findUser(loginUserDto:LoginUserDto){
+        try{
+            const {login, password} = loginUserDto;
+            const user = await this.repository.findOne({
+                where: [
+                    { email: login, password: password },
+                    { name: login, password: password },
+                    { phone_number: login, password: password }
+                ]
+            });
+
+            return user;
+
+        }catch(error:any){
+            console.error('Can not find user!', error.message)
+            throw error;
+        }
+    }
+
 }
