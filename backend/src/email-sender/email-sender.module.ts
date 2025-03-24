eaf1682db960e3c0ff41495d24f42af4168004ce
vorogurcov <vania.frol2010@yandex.ru>
2025-03-24 11:21:20 +0300

    Refactor profile module to have only one root for profile

--- backend/src/email-sender/email-sender.module.ts
+++ backend/src/email-sender/email-sender.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EmailSenderService } from './services/email-sender.service';
 import {RedisModule} from "../redis/redis.module";
+import {EmailVerificationService} from "./services/email-verification.service";
 
 @Module({
     imports: [RedisModule],
-    providers: [EmailSenderService],
-    exports: [EmailSenderService],
+    providers: [EmailSenderService,EmailVerificationService],
+    exports: [EmailSenderService,EmailVerificationService],
 })
 export class EmailSenderModule {}
