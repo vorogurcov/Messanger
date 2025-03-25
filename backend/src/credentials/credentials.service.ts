e57c2b583b05e0cb5419627710dc9ddb6717cc2f
vorogurcov <vania.frol2010@yandex.ru>
2025-03-25 15:14:23 +0300

    Improve credentials module

--- backend/src/credentials/credentials.service.ts
+++ backend/src/credentials/credentials.service.ts
@@ -1,6 +1,8 @@
 import {Injectable, NotFoundException} from "@nestjs/common";
 import {RegisterUserDto} from "../auth/dto/register-user.dto";
 import {UserCredentialsRepository} from "./repositories/user-credentials.repository";
+import {UpdateCredentialsDto} from "../profile/dto/update-credentials.dto";
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class CredentialsService {
@@ -26,6 +28,29 @@ export class CredentialsService {
             throw new NotFoundException(`User with ID ${id} not found`);
         }
         user.isEmailVerified = value;
+        return this.repo.save(user);
+    }
+
+    async updateCredentials(id: string, updateCredentialsDto:UpdateCredentialsDto ) {
+        const user = await this.repo.findById(id);
+        if (!user) {
+            throw new NotFoundException(`User with ID ${id} not found`);
+        }
+
+
+        if (updateCredentialsDto.email) {
+            user.email = updateCredentialsDto.email;
+            user.isEmailVerified = false
+        }
+
+        if (updateCredentialsDto.password) {
+            const saltRounds = 10;
+            const hashedPassword = await bcrypt.hash(updateCredentialsDto.password, saltRounds);
+            user.password = hashedPassword;
+        }
+
+
+
         return this.repo.save(user);
     }
 }
