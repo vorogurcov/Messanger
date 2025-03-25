18832b04c452661f0611d1b1e44627246ecda101
vorogurcov <vania.frol2010@yandex.ru>
2025-03-25 15:14:06 +0300

    Create new method and change DTO name in email-sender

--- backend/src/email-sender/services/email-sender.service.ts
+++ backend/src/email-sender/services/email-sender.service.ts
@@ -48,4 +48,38 @@ export class EmailSenderService {
             console.log('Email was not send due ', error);
         }
     }
+
+    async sendProfileUpdateVerificationEmail(
+        email: string,
+        login: string,
+        confirmationCode: string,
+    ) {
+        try {
+            const info = await this.transporter.sendMail({
+                from: `"Messanger Bot" <${process.env.MAIL_GMAIL_ACCOUNT}>`,
+                to: email,
+                subject: 'Profile Change Verification',
+                text: `Hello, ${login}!\nYou've received this message because you are trying to change your profile data.\nPlease use this confirmation code to proceed: ${confirmationCode}`,
+                html: `<div style="font-family: Arial, sans-serif; font-size: 16px; color: #333; background-color: #f9f9f9; padding: 30px; border-radius: 10px; max-width: 600px; margin: auto; border: 1px solid #ddd;">
+      <h2 style="color: #2c3e50; text-align: center;">Profile Update Confirmation</h2>
+      <p>Hello, <strong>${login}</strong>!</p>
+      <p>You’ve received this message because you initiated changes to your personal data in your account.</p>
+      <p style="margin-top: 20px;">Please use the confirmation code below to verify and proceed with these changes:</p>
+      <div style="text-align: center; margin: 30px 0;">
+        <span style="display: inline-block; background-color: #007BFF; color: #fff; padding: 15px 25px; font-size: 20px; font-weight: bold; border-radius: 8px; letter-spacing: 2px;">
+          ${confirmationCode}
+        </span>
+      </div>
+      <p>If you did not request to change your data, you can safely ignore this message and no changes will be made.</p>
+      <hr style="margin: 40px 0; border: none; border-top: 1px solid #ccc;" />
+      <p style="font-size: 14px; color: #888;">This is an automated message. Please do not reply.</p>
+    </div>`,
+            });
+
+            console.log('Profile update verification message sent: %s', info.messageId);
+        } catch (error) {
+            console.log('Profile update verification email was not sent due to:', error);
+        }
+    }
+
 }
--- backend/src/email-sender/services/email-verification.service.ts
+++ backend/src/email-sender/services/email-verification.service.ts
@@ -1,29 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { EmailConfirmationDto } from '../dto/email-confirmation.dto';
+import {ConfirmationDto} from '../dto/email-confirmation.dto';
 import { RedisService } from '../../redis/redis.service';
 @Injectable()
 export class EmailVerificationService {
     constructor(private redisService: RedisService) {}
 
-    async generateAndSaveCode(userId: string): Promise<string> {
+    async generateAndSaveCode(actionKey:string,userId: string): Promise<string> {
         const code = Math.floor(100000 + Math.random() * 900000).toString();
         await this.redisService.set(
-            `email_verification_for_id:${userId}`,
+            `${actionKey}_for_id:${userId}`,
             code,
             600,
         );
         return code;
     }
 
-    async verifyCode(
-        emailConfirmationDto: EmailConfirmationDto,
+    async verifyCode(actionKey:string,
+        confirmationDto: ConfirmationDto,
     ): Promise<boolean> {
-        const { confirmationCode, userId } = emailConfirmationDto;
+        const { confirmationCode, userId } = confirmationDto;
         const storedCode = Number(
-            await this.redisService.get(`email_verification_for_id:${userId}`),
+            await this.redisService.get(`${actionKey}_for_id:${userId}`),
         );
         if (storedCode === confirmationCode) {
-            await this.redisService.del(`email_verification_for_id:${userId}`);
+            await this.redisService.del(`${actionKey}_for_id:${userId}`);
             return true;
         }
         return false;
