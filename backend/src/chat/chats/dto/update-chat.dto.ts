58b099875c891825c272178714107144847e80e7
vorogurcov <vania.frol2010@yandex.ru>
2025-04-02 14:30:41 +0300

    Create dtos for createChat and updateChat actions

---
+++ backend/src/chat/chats/dto/update-chat.dto.ts
@@ -0,0 +1,23 @@
+import { IsOptional, IsString, IsUUID, ArrayNotEmpty, ArrayUnique } from 'class-validator';
+import {Transform} from "class-transformer";
+
+export class UpdateChatDto {
+    @IsOptional()
+    @IsString()
+    type?: string;
+
+    @IsOptional()
+    @IsString()
+    name?: string;
+
+    // @IsOptional()
+    // @IsUUID()
+    // chatOwnerId?: string;
+
+    @IsOptional()
+    @ArrayNotEmpty()
+    @ArrayUnique()
+    @IsUUID('4', { each: true })
+    @Transform(({ value }) => Array.isArray(value) ? value : [value])
+    userIds?: string[];
+}
