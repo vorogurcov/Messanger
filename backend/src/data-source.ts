b6b7e98973c1570e0b9e4d818fea9b1b662b2438
vorogurcov <vania.frol2010@yandex.ru>
2025-04-09 20:14:33 +0300

    Implement migrations

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,10 +1,12 @@
 import { DataSource } from 'typeorm';
 import { UserAuth } from './user/credentials/entities/user-auth.entity';
 import { UserProfile } from './user/profile/entities/user-profile.entity';
-// import { UserProfileAndAuth1742557853253 } from './common/migration/schema/1742557853253-UserProfileAndAuth';
-// import { UserProfileAndAuthSeed1742557865532 } from './common/migration/seed/1742557865532-UserProfileAndAuthSeed';
+import { UserProfileAndAuth1742557853253 } from './common/migration/schema/1742557853253-UserProfileAndAuth';
+import { UserProfileAndAuthSeed1742557865532 } from './common/migration/seed/1742557865532-UserProfileAndAuthSeed';
 import { Message } from './chat/messages/entities/message.entity';
 import { Chat } from './chat/chats/entities/chat.entity';
+import {ChatsAndMessages1744218222284} from "./common/migration/schema/1744218222284-ChatsAndMessages";
+import {ChatsAndMessagesSeed1744218244396} from "./common/migration/seed/1744218244396-ChatsAndMessagesSeed";
 
 export const AppDataSource = new DataSource({
     host: process.env.DB_HOST,
@@ -13,10 +15,12 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    synchronize: true,
+    synchronize: false,
     entities: [UserAuth, UserProfile, Chat, Message],
-    // migrations: [
-    //     UserProfileAndAuth1742557853253,
-    //     UserProfileAndAuthSeed1742557865532,
-    // ],
+     migrations: [
+         UserProfileAndAuth1742557853253,
+         UserProfileAndAuthSeed1742557865532,
+         ChatsAndMessages1744218222284,
+         ChatsAndMessagesSeed1744218244396
+     ],
 });
